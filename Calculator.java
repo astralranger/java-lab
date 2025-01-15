319cacf35f2b039915ca7ae2b55af644c556cb5a
Piyush Gawade <152266274+astralranger@users.noreply.github.com>
2025-01-15 16:00:33 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,34 @@
+// Calculator.java
+class Calculator {
+  public int addition(int[] num) {
+
+    int sum = num[0] + num[1];
+
+    return sum;
+
+  }
+
+  public int subtraction(int[] num) {
+
+    int sub = num[0] - num[1];
+
+    return sub;
+
+  }
+
+  public int multiplication(int[] num) {
+
+    int mul = num[0] * num[1];
+
+    return mul;
+
+  }
+
+  public int division(int[] num) {
+
+    int div = num[0] / num[1];
+
+    return div;
+
+  }
+}
