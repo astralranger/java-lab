9e8adbfddbd72f7fb017353a3c36d9a7d313609f
astralranger <interstellar.ranger@gmail.com>
2025-02-03 23:38:07 +0530

    modified code

---
+++ Array_Lists/MainProgram.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.*;
+
+public class MainProgram {
+    public static void main(String[] args) {
+        UserInput userInput = new UserInput();
+        DisplayArray displayArray = new DisplayArray();
+
+        ArrayList<Integer> arr = userInput.arrayInput();
+
+        displayArray.display(arr);
+        displayArray.oddEven(arr);
+        int smallestDiff = displayArray.smallestConsecutiveDifference(arr);
+
+        System.out.println("Smallest consecutive difference is: " + smallestDiff);
+    }
+}
