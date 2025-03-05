0c506c0feb977f9d1d016a26db310b544dd00636
astralranger <interstellar.ranger@gmail.com>
2025-03-06 00:42:30 +0530

    Updates and added comments to StudentOperation.java and Main.java

--- Assignment_7/Main.java
+++ Assignment_7/Main.java
@@ -1,8 +1,8 @@
-
-public class Main{
-    public static void main(String[] args){
-        StudentOperations studentOperations = new StudentOperations();
-        StudentOperations.insertStudent();
-        StudentOperations.viewStudents();
+public class Main {
+    public static void main(String[] args) {
+        // no need to create an instance because methods are static
+        StudentOperations.insertStudent(); // call method to insert student data
+        StudentOperations.viewStudents();  // call method to view all student records
     }
 }
+
