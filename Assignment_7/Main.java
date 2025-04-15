c0f03a887228e0b8990d72f75c0770120778cb51
astralranger <interstellar.ranger@gmail.com>
2025-04-15 21:46:14 +0530

    Java file bulk commits

--- Assignment_7/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        // no need to create an instance because methods are static
-        StudentOperations.insertStudent(); // call method to insert student data
-        StudentOperations.viewStudents();  // call method to view all student records
-    }
-}
-
